2a835661058420489db013f7c0fa87837295f54e
cs <cs@enure.net>
2009-08-16 18:23:34 -0700

    re-factoring as a jquery plugin

--- hideAllExcept.js
+++ hideAllExcept.js
@@ -1,20 +1,26 @@
 $(document).ready(function() {
-    var hash = window.location.hash;
-    
-    (!hash) ?  
-        hideAllExcept('#' + $('#toggleThis > div:first').attr('id')) 
-            : hideAllExcept(window.location.hash);
-
-    $('a.toggle').click(function() {
-        var href = $(this).attr('href');
-        hideAllExcept(href);
-        return false; // prevents the window from scrolling to the anchor
-    });
+    $().hideAllExcept.init();
 });
 
-function hideAllExcept(el) {
-    $(el).removeClass('hide').siblings().addClass('hide');
+$.fn.hideAllExcept = {
+    init: function() {
+        var hash = window.location.hash;
+
+        (!hash) ?  
+            $().hideAllExcept.hideShow('#' + $('#toggleThis > div:first').attr('id')) 
+                : $().hideAllExcept.hideShow(window.location.hash);
+
+        $('a.toggle').click(function() {
+            var href = $(this).attr('href');
+            $().hideAllExcept.hideShow(href);
+            return false; // prevents the window from scrolling to the anchor
+        });
+        
+    },
+    hideShow: function(el) {
+        $(el).removeClass('hide').siblings().addClass('hide');
     
-    $('a.toggle').removeClass('active');
-    $('a[href="' + el + '"]').addClass('active');
+        $('a.toggle').removeClass('active');
+        $('a[href="' + el + '"]').addClass('active');
+    }
 }
