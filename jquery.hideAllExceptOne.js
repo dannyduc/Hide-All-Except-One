9de5a9f200fd2562ce5bc78ca26e09e55b8a7bae
cs <cs@enure.net>
2009-11-09 08:21:44 -0800

    change from object to function. adding min.js version. change css.

--- jquery.hideAllExceptOne.js
+++ jquery.hideAllExceptOne.js
@@ -1,31 +1,26 @@
 /* http://github.com/enure/Hide-All-Except-One */
 
 (function($){
-  $.hideAllExcept = {
-    init: function() {
+  $.hideAllExcept = function(){
+    function init() {
       var hash = window.location.hash;
-      var hs = $.hideAllExcept.hideShow;
 
-      (!hash) ? hs('#' + $('#toggleThis > div:first').attr('id')) : hs(window.location.hash);
+      // make it bookmarkable/refreshable. but, no history.
+      (!hash) ? hideShow('#' + $('#toggleThis > div:first').attr('id')) : hideShow(window.location.hash);
 
+      // add click handler.
       $('a.toggle').click(function(e) {
         e.preventDefault();
         var href = $(this).attr('href');
-        hs(href);
+        hideShow(href);
       });
-    },
-    hideShow: function(el) {
+    }
+    function hideShow(el) {
       $(el).show().siblings().hide();
 
       $('a.toggle').removeClass('active');
       $('a[href="' + el + '"]').addClass('active');
     }
-  }
+    init();
+  };
 })(jQuery);
-
-
-
-
-
-
-
